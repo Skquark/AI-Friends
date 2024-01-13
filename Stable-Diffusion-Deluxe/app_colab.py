45083e5446d1cbf839173ee38ba919eda656cf49
Alan Bedian <skquark@yahoo.com>
2024-01-12 19:14:15 -0800

    Add arg FLAGS to pass variables

--- Stable-Diffusion-Deluxe/app_colab.py
+++ Stable-Diffusion-Deluxe/app_colab.py
@@ -1,9 +1,17 @@
-global storage_type, save_to_GDrive, saved_settings_json, tunnel_type, auto_launch_website
+import os, subprocess, sys, shutil, re
 
+FLAGS = {}
+for arg in sys.argv[1:]:
+  key, value = arg.split("=")
+  FLAGS[key] = value
+storage_type = FLAGS.get('--storage_type')
+save_to_GDrive = FLAGS.get('--save_to_GDrive') == "True"
+saved_settings_json = FLAGS.get('--saved_settings_json')
+tunnel_type = FLAGS.get('--tunnel_type')
+auto_launch_website = FLAGS.get('--auto_launch_website') == "True"
 force_updates = True
 newest_flet = True
 SDD_version = "v1.9.0"
-import os, subprocess, sys, shutil, re
 import random as rnd
 from IPython.display import clear_output
 root_dir = '/content/'
