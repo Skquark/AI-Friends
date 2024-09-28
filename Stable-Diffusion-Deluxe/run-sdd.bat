fc0615676981b52d270deba5baca08f77576058a
Alan Bedian <skquark@yahoo.com>
2024-09-28 03:30:00 -0700

    Refactor pip_install and pip.exe from venv. Other minor fixes/updates.

--- Stable-Diffusion-Deluxe/run-sdd.bat
+++ Stable-Diffusion-Deluxe/run-sdd.bat
@@ -8,7 +8,6 @@ reg query "hkcu\software\Python" >nul
 if ERRORLEVEL 1 GOTO NOPYTHON
 echo Downloading latest Stable Diffusion Deluxe and running in a Python Virtual Environment
 if not EXIST .\venv (py -3 -m venv .\venv)
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Skquark/AI-Friends/main/Stable-Diffusion-Deluxe/Stable-Diffusion-Deluxe.py -OutFile .\venv\Stable-Diffusion-Deluxe.py"
 powershell -Command "if (Test-Connection -ComputerName google.com -Count 1 -Quiet) { Invoke-WebRequest https://raw.githubusercontent.com/Skquark/AI-Friends/main/Stable-Diffusion-Deluxe/Stable-Diffusion-Deluxe.py -OutFile .\venv\Stable-Diffusion-Deluxe.py } else { if (Test-Path '.\venv\Stable-Diffusion-Deluxe.py') { Write-Host 'No internet connection. Using existing script version.' } else { Write-Host 'No internet connection on first run. Unable to proceed.' ; exit 1 } }"
 cd .\venv
 call .\Scripts\activate.bat
